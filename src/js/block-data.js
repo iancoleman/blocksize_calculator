3da4d6138aca8276064f886812929ee2a725eef9
Ian Coleman <coleman.ian@gmail.com>
2016-06-10 11:12:41 +1000

    Charts use csv data source

--- src/js/block-data.js
+++ src/js/block-data.js
@@ -1,7 +1,5 @@
 (function() {
 
-    var self = this;
-
     var data = [];
 
     var DOM = {};
@@ -23,64 +21,86 @@
     }
 
     function showDataSize() {
-        fetch("/data/blocks.json", {method: 'HEAD'})
-            .then(function(response) {
-                var sizeBytes = response.headers.get("content-length");
-                var sizeMb = (sizeBytes / 1024 / 1024).toFixed(1);
-                DOM.size.textContent = sizeMb + " MB";
-            });
+        var request = new XMLHttpRequest();
+        request.open("HEAD", "/data/blocks.csv", true);
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    var sizeBytes = request.getResponseHeader("Content-Length");
+                    var sizeMb = (sizeBytes / 1024 / 1024).toFixed(1);
+                    DOM.size.textContent = sizeMb + " MB";
+                }
+            }
+        }
+        request.send(null);
     }
 
     function loadData() {
+
         // Hide load button
         DOM.load.classList.add("hidden");
         // Show loading
         DOM.loading.classList.remove("hidden");
-        fetch("data/blocks.json")
-            .then(function(response) {
-                if (!response.ok) {
+        // Load the data
+        Papa.parse("/data/blocks.csv", {
+            download: true,
+            complete: function(result) {
+                if (result.errors.length > 0) {
                     // TODO report error
                     return;
                 }
-                response.json()
-                    .then(function(data) {
-                        self.data = data;
-                        // Hide loading
-                        DOM.loading.classList.add("hidden");
-                        // Show charts
-                        DOM.charts.classList.remove("hidden");
-                        // Set min and max dates
-                        var minDateStr = dateStr(data[0].time);
-                        var maxDateStr = dateStr(data[data.length-1].time);
-                        DOM.start.setAttribute("min", minDateStr);
-                        DOM.start.setAttribute("max", maxDateStr);
-                        DOM.start.setAttribute("value", minDateStr);
-                        DOM.start.addEventListener("input", render);
-                        DOM.end.setAttribute("min", minDateStr);
-                        DOM.end.setAttribute("max", maxDateStr);
-                        DOM.end.setAttribute("value", maxDateStr);
-                        DOM.end.addEventListener("input", render);
-                        render();
-                    });
-                });
+                // Parse csv data into array of block objects
+                var rows = result.data;
+                var columnNames = rows[0];
+                for (var i=1; i<rows.length; i++) {
+                    var cells = rows[i];
+                    if (cells.length != 4) {
+                        continue;
+                    }
+                    var block = {};
+                    for (var j=0; j<columnNames.length; j++) {
+                        var key = columnNames[j];
+                        var value = parseFloat(cells[j]);
+                        block[key] = value;
+                    }
+                    data.push(block);
+                }
+                // Hide loading
+                DOM.loading.classList.add("hidden");
+                // Show charts
+                DOM.charts.classList.remove("hidden");
+                // Set min and max dates
+                var minDateStr = dateStr(data[0].time);
+                var maxDateStr = dateStr(data[data.length-1].time);
+                DOM.start.setAttribute("min", minDateStr);
+                DOM.start.setAttribute("max", maxDateStr);
+                DOM.start.setAttribute("value", minDateStr);
+                DOM.start.addEventListener("input", render);
+                DOM.end.setAttribute("min", minDateStr);
+                DOM.end.setAttribute("max", maxDateStr);
+                DOM.end.setAttribute("value", maxDateStr);
+                DOM.end.addEventListener("input", render);
+                render();
+            },
+        })
     }
 
     function render() {
         // Size chart values
         sizeLabels = [];
         sizeValues = [];
-        minSize = 0; // kB
+        minSize = 0; // KB
         sizeStep = 100;
-        maxSize = 1000; // kB
+        maxSize = 1000; // KB
         for (var kb=minSize; kb<=maxSize; kb+=sizeStep) {
-            sizeLabels.push(kb.toFixed(0) + "-" + (kb+sizeStep).toFixed(0) + " kb");
+            sizeLabels.push(kb.toFixed(0) + "-" + (kb+sizeStep).toFixed(0) + " KB");
             sizeValues.push(0);
         }
-        sizeLabels[sizeLabels.length-1] = (maxSize) + " kb+";
+        sizeLabels[sizeLabels.length-1] = (maxSize) + " KB+";
         // Txlength chart values
         txlengthLabels = [];
         txlengthValues = [];
-        minTxlength = 0; // kB
+        minTxlength = 0; // KB
         var maxTxlength = 3000;
         txlengthStep = Math.ceil(maxTxlength / 10);
         for (var txs=0; txs<=maxTxlength; txs+=txlengthStep) {
@@ -108,8 +128,8 @@
         // Parse the data
         var start = new Date(DOM.start.value).getTime() / 1000;
         var end = new Date(DOM.end.value).getTime() / 1000;
-        for (var i=0; i<self.data.length; i++) {
-            var block = self.data[i];
+        for (var i=0; i<data.length; i++) {
+            var block = data[i];
             // Ignore data outside the time range
             if (block.time < start || block.time > end) {
                 continue;
@@ -131,7 +151,7 @@
             cumSizetxs += txlength;
             // Timing
             if (i > 0) {
-                var prevBlock = self.data[i-1];
+                var prevBlock = data[i-1];
                 var timing = block.time - prevBlock.time;
                 if (timing > maxTiming) {
                     timing = maxTiming;
